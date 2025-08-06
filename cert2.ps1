0895569f1ab2af8d889f29ea1a55a7f9d313229c
Korarski2028 <ekelm2004@gmail.com>
2025-08-05 23:57:05 -0300

    Add files via upload

---
+++ cert2.ps1
@@ -0,0 +1,12 @@
+﻿Connect-VIServer -Server $Vc -Credential $Creds 
+
+$certMgr = Get-View -Id CertificateManager-certificateManager
+$certInfos = $certMgr.CertificateInfoList
+
+foreach ($certInfo in $certInfos) {
+    [PSCustomObject]@{
+        Subject = $certInfo.Subject
+        Issuer = $certInfo.Issuer
+        Expiration = $certInfo.NotAfter
+    }
+} | Format-Table -AutoSize
