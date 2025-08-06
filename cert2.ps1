72b0cbf0e70174072abd377e4315b17f9fdaf3c2
Korarski2028 <ekelm2004@gmail.com>
2025-08-06 00:01:53 -0300

    Add files via upload

--- cert2.ps1
+++ cert2.ps1
@@ -1,4 +1,6 @@
-﻿Connect-VIServer -Server $Vc -Credential $Creds 
+﻿#########################################################################
+# Method 1
+Connect-VIServer -Server $Vc -Credential $Creds 
 
 $certMgr = Get-View -Id CertificateManager-certificateManager
 $certInfos = $certMgr.CertificateInfoList
@@ -10,3 +12,23 @@ foreach ($certInfo in $certInfos) {
         Expiration = $certInfo.NotAfter
     }
 } | Format-Table -AutoSize
+
+#########################################################################
+# Method 2
+# Connect to vCenter
+Connect-VIServer -Server $Vc -Credential $Creds 
+
+# get certificates >>> REST API
+$headers = @{
+    "vmware-api-session-id" = $global:DefaultVIServer.SessionSecret
+}
+$uri = "https://$Vc/api/vcenter/certificate-management/vcenter/tls"
+
+try {
+    $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers -ContentType "application/json"
+    $response | Select-Object issuer, subject, valid_to | Format-Table -AutoSize
+} catch {
+    Write-Host "Error accessing API: $_"
+}
+
+Disconnect-VIServer -Server $Vc -Confirm:$false
