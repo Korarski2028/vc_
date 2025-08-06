24541a18c4d7a6d504c4bc2fc57722faed68d8c5
Korarski2028 <ekelm2004@gmail.com>
2025-08-06 00:23:54 -0300

    Add files via upload

--- cert2.ps1
+++ cert2.ps1
@@ -15,7 +15,7 @@ foreach ($certInfo in $certInfos) {
 
 #########################################################################
 # Method 2
-# Connect to vCenter
+# Connect to vCenter and use rest API 
 Connect-VIServer -Server $Vc -Credential $Creds 
 
 # get certificates >>> REST API
@@ -31,4 +31,26 @@ try {
     Write-Host "Error accessing API: $_"
 }
 
+# Get other  certificate via API
+$headers = @{
+    "Accept" = "application/json"
+}
+
+$uri = "https://$Vc/api/vcenter/certificate-management/vcenter/trusted-root-chains"
+
+try {
+    $certificates = Invoke-RestMethod -Uri $uri -Headers $headers -Credential $Creds -Method Get
+    $certificates | Format-List
+} catch {
+    Write-Error "Failed to retrieve certificates: $_"
+}
+
 Disconnect-VIServer -Server $Vc -Confirm:$false
+########################################################
+# solution cert 
+Connect-VIServer -Server $Vc -Credential $Creds 
+# Get all solution certificates
+Get-SsoSolutionCertificate
+
+# Get specific solution certificate details
+Get-SsoSolutionCertificate -Name "vsphere-webclient"
